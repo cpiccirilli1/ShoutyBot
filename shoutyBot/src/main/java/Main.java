1cc6c2d41aba9a328bb1afd35e196600e820e33e
cpiccirilli1 <Piccirilli115@gmail.com>
2020-09-23 18:08:10 -0400

    updated login error handling

--- shoutyBot/src/main/java/Main.java
+++ shoutyBot/src/main/java/Main.java
@@ -1,4 +1,6 @@
 import net.dv8tion.jda.api.*;
+
+import javax.security.auth.login.LoginException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -22,13 +24,16 @@ public class Main {
     }
 
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws LoginException, InterruptedException{
         String fn = "C:\\Users\\Chelsea\\Documents\\tokens\\shoutyToken.txt";
         String token = getToken(fn);
+        if (!token.equals("null")) {
+            JDA jda = new JDABuilder(token).build();
 
-        JDA jda = new JDABuilder(token).build();
 
-
-        jda.awaitReady();
+            jda.awaitReady();
+        } else{
+            System.out.println("Token was bogus or missing.");
+        }
     }
 }
